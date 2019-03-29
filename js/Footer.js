d1ed4bbe43db235fdf4bff8c13bbbe47be4a1bb5
Ryan Carville <ryancarville@gmail.com>
2019-03-29 10:16:58 +0100

    Updated footer

--- js/Footer.js
+++ js/Footer.js
@@ -1,7 +1,13 @@
 function footer() {
 	$('.footer').append(
 		`<div id='footer'>
-				<p>PlaceHolder for links in footer</p>
-			</div>`
+			<a onclick=welcome()>Home</a>	
+			<a onclick=office()>Office</a>
+			<a onclick=realEstate()>Real Estate</a>
+			<a onclick=buy(properties)>Buy</a>
+			<a onclick=rent(properties)>Rent</a>
+			<a onclick=sell()>Sell</a>
+			<a onclick=contact()>Contact</a>
+		</div>`
 	);
 }
