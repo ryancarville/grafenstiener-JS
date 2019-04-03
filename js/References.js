d778d653c8b1346f78034d47964a731fad4e54ed
Ryan Carville <ryancarville@gmail.com>
2019-04-03 18:48:26 +0200

    updates

---
+++ js/References.js
@@ -0,0 +1,29 @@
+function references() {
+	const julia = 'julia';
+	$('.main').empty();
+	$('.main').append(
+		`<div class='refWrapper'>
+            <div class='refHeader'>
+                <h2>Clients who love us</h2>
+            </div>
+            <div class='refImages'>
+                <div class='refImg' onClick='$(refPage(1))'>
+                <img src='images/references/1.jpg' alt='referncesImg' />
+                <a>Julia Castro</a>
+                </div>
+                <div class='refImg' onClick='$(refPage(2))'>
+                <img src='images/references/2.jpg' alt='referncesImg' />
+                <a>Sam Golden</a>
+                </div>
+                <div class='refImg' onClick='$(refPage(3))'>
+                    <img src='images/references/3.jpg' alt='referncesImg' />
+                    <a>James Smith</a>
+                </div>
+                <div class='refImg' onClick='$(refPage(4))'>
+                    <img src='images/references/4.jpg' alt='referncesImg' />
+                    <a>Sandra Hull</a>
+                </div>
+            </div>
+        </div>`
+	);
+}
---
+++ js/refPage.js
@@ -0,0 +1,91 @@
+function refPage(client) {
+	if (client == 1) {
+		$('.main').empty();
+		$('.main').append(
+			`<div class='refPageWrap'>
+            <div class='refContent'>
+                <div class='refPageImg'>
+                    <img src='images/references/1.jpg' alt='references image'/>
+                </div>
+                <div class='refText'>
+                <h3>Julia Castro</h3>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut.<br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+                    </p>
+                </div>
+            </div>
+        </div>`
+		);
+	} else if (client == 2) {
+		$('.main').empty();
+		$('.main').append(
+			`<div class='refPageWrap'>
+            <div class='refContent'>
+                <div class='refPageImg'>
+                    <img src='images/references/2.jpg' alt='references image'/>
+                </div>
+                <div class='refText'>
+                <h3>Sam Golden</h3>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut.<br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+                    </p>
+                </div>
+            </div>
+        </div>`
+		);
+	} else if (client == 3) {
+		$('.main').empty();
+		$('.main').append(
+			`<div class='refPageWrap'>
+            <div class='refContent'>
+                <div class='refPageImg'>
+                    <img src='images/references/3.jpg' alt='references image'/>
+                </div>
+                <div class='refText'>
+                <h3>James Smith</h3>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut.<br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+                    </p>
+                </div>
+            </div>
+        </div>`
+		);
+	} else if (client == 4) {
+		$('.main').empty();
+		$('.main').append(
+			`<div class='refPageWrap'>
+            <div class='refContent'>
+                <div class='refPageImg'>
+                    <img src='images/references/4.jpg' alt='references image'/>
+                </div>
+                <div class='refText'>
+                <h3>Sandra Hull</h3>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut.<br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+                    </p>
+                </div>
+            </div>
+        </div>`
+		);
+	}
+}
