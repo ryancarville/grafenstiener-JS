fb9b14d02fbd7b3e5cddd08193d1358570be78fb
Ryan Carville <ryancarville@gmail.com>
2019-05-19 19:28:11 +0200

    History Update

--- js/main.js
+++ js/main.js
@@ -20,7 +20,22 @@ $(document).ready(() => {
 	nav = $('nav#navLinks');
 	content = $('main#main');
 
-	fetchAndInsert = () => {};
+	//fetches and inserts content in container
+	fetchAndInsert = href => {
+		$.ajax({
+			url:
+				'file:///Users/ryancarville/Desktop/Gr%C3%A4fensteinerRealEstate/pureJSSite/' +
+				href,
+			method: 'GET',
+			cache: false,
+			success: () => {
+				content.html(data);
+			},
+			error: () => {
+				return 'File could not be found.';
+			}
+		});
+	};
 
 	nav.find('a').on('click', e => {
 		var href = $(this).attr('href');
@@ -28,6 +43,9 @@ $(document).ready(() => {
 
 		//Manipulate History
 		history.pushState(null, null, href);
+
+		//Fetch and insert content
+		fetchAndInsert(href);
 		e.preventDefault();
 	});
 });
