f8b04e5fd8fbc4b45cef233c5e152e456da7f7c8
Ryan Carville <ryancarville@gmail.com>
2019-05-21 17:47:01 +0200

    update home link for history

--- js/main.js
+++ js/main.js
@@ -17,21 +17,22 @@ $(document).ready(() => {
 	});
 });
 
+const route = 'welcome';
+window.history.pushState({ route }, '', '/');
+
 $('#desktopLinks li a').on('click', function(e) {
 	let route = $(this).attr('route');
 	console.log(route);
 	window.history.pushState({ route }, '', `/${route}`);
 	console.log(history.state);
 });
-const route = 'welcome';
-window.history.pushState({ route }, '', '/');
 
 window.onpopstate = function() {
 	console.log(history.state);
 	let route = history.state['route'];
 	console.log(route);
 	switch (route) {
-		case 'welcome':
+		case '':
 			welcome();
 			break;
 		case 'office':
